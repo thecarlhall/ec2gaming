3723d81debc0b6d894b824e21cf10caf0bd5d1ee
Carl Hall <carl@halls.farm>
2021-12-10 16:28:37 -0500

    Look for stopped instance when describing a relevant instance

--- scripts/ec2gaming-instance.sh
+++ scripts/ec2gaming-instance.sh
@@ -2,7 +2,7 @@
 source "$(dirname "$0")/ec2gaming.header"
 
 # Verify that the gaming stane actually exists (and that there's only one)
-INSTANCES=$(aws ec2 describe-instances --filters Name=instance-state-code,Values=16 Name=instance-type,Values="$INSTANCE_TYPE")
+INSTANCES=$(aws ec2 describe-instances --filters Name=instance-state-code,Values=[16,80] Name=instance-type,Values="$INSTANCE_TYPE")
 if [ "$(echo "$INSTANCES" | jq '.Reservations | length')" -ne "1" ]; then
     >&2 echo "didn't find an instance or there wasn't exactly one $INSTANCE_TYPE instance"
     exit 1
